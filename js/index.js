eb360bce5a1ae54b5ed462d868bbc5d75bf495fd
adamsienko <adamsienko1@gmail.com>
2017-10-04 15:25:56 +0200

    new dots

--- js/index.js
+++ js/index.js
@@ -1,45 +1,6 @@
-'use strict';
-//DOTS
-
-/*
-jQuery(document).ready(function($){
-	var contentSections = $('.cd-section'),
-		navigationItems = $('#cd-vertical-nav a');
-
-	updateNavigation();
-	$(window).on('scroll', function(){
-		updateNavigation();
-	});
-
-	
-    //smooth scroll down
-    $('.cd-scroll-down').on('click', function(event){
-        event.preventDefault();
-        smoothScroll($(this.hash));
-    });
-    
-	function updateNavigation() {
-		contentSections.each(function(){
-			$this = $(this);
-			var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
-			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
-				navigationItems.eq(activeSection).addClass('is-selected');
-			}else {
-				navigationItems.eq(activeSection).removeClass('is-selected');
-			}
-		});
-	}
-
-	function smoothScroll(target) {
-        $('body,html').animate(
-        	{'scrollTop':target.offset().top},
-        	600
-        );
-	}
-});
-*/
-
 $(function () {
+//    DOTS
+
 
     //SIEDEBAR
 
@@ -89,3 +50,4 @@ $(document).ready(function () {
 
     });
 });
+
